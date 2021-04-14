c04d1460a75330839a3cc5a8d29f074c119f4be5
Matius Rimbe <matiusrimbe.ptik@qmail.id>
2021-04-14 22:56:32 +0800

    first commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable object-curly-newline */
+/* eslint-disable import/newline-after-import */
+
+const notes = [];
+
+module.exports = notes;
